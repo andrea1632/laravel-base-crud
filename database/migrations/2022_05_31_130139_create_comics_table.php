87f9f7de23325a703c385e1c10e1f91edd5b8b8a
andrea1632 <andrea.bellucci10@libero.it>
2022-05-31 15:11:13 +0200

    added cmoics migration, controller & model

---
+++ database/migrations/2022_05_31_130139_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('thumb');
+            $table->float('price', 8, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->enum('type', ['graphic novel', 'comic book']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
